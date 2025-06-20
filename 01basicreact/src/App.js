324abe3cac446f5260cb812f2268ce84ffaa3725
Anubhav Vishwakarma <anubhavvishwakarma17@gmail.com>
2025-06-20 22:06:20 +0530

    second lecture

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <h1>
+      chai aur react | react and vite project kaise banta hai pta chala
+    </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <h1>
+      Hello duniya
+    </h1>
+  )
+}
+
+export default App
