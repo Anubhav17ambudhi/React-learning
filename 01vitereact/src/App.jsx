3f3d9e1bcb8ee4c062910e6c971771dcd364a178
Anubhav Vishwakarma <anubhavvishwakarma17@gmail.com>
2025-06-26 17:10:59 +0530

    underhood of react and a custom react with actuall how the thing work in react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,17 @@
 import Chai from "./chai.jsx"
 
 function App() {
+  const username = "Chai aur code"
   return (
     <>
       <Chai/>
+      <h1>Chai aur {username}</h1>
     </>
   )
 }
+//{} - this represents a final value evaluated through javascript
+// this will be a evaluated expression 
+// no if for loops etc 
 
 //actually jsx wants this to return only single element
 //so therefore we have <></> fragments in this
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,11 +1,41 @@
-import { StrictMode } from 'react'
+import React,{ StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function Myapp(){
+  return (
+    <div>
+      <h1>Custom App!!</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//     href: 'https://google.com',
+//     target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+const anotherUser = "Chai aur sirf Chai"
+
+const ReactElement = React.createElement(
+  'a',
+  {href:'https://google.com',target: '_blank'},
+  'click me to visit google',
+  anotherUser//this will directly be inject as a evaluated expression
+)
+
 ReactDOM.createRoot(document.getElementById('root')).
 render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+    ReactElement
+  
 )
 //strictMode can be removed easily
