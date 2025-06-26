1d98316d204d617d97e89896dfbe98f699b680a2
Anubhav Vishwakarma <anubhavvishwakarma17@gmail.com>
2025-06-26 21:10:02 +0530

    some comments here and there

--- 02counter/src/main.jsx
+++ 02counter/src/main.jsx
@@ -8,3 +8,9 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+//createRoot also creates a DOM
+//compares this and the actual DOM and then updates the things that actually gets
+//updated in the UI 
+//when browser Reloads the new DOM 
+//virtual dom tracks the changed elements and updates them only
