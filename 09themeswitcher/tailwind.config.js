d5acdc05a91668bb0f829fe784ed8f062c8ff12d
Anubhav Vishwakarma <anubhavvishwakarma17@gmail.com>
2025-07-17 07:38:58 +0530

    tailwindconfig

--- 09themeswitcher/tailwind.config.js
+++ 09themeswitcher/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
